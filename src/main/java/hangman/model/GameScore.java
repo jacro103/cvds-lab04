f4ffd78e244738c764cd58e68937ba8adba9f07c
unknown <sebastian.blanco@labinfo.is.escuelaing.edu.co>
2023-02-16 17:20:52 -0500

    Se agregan los comentarios de las clases

--- src/main/java/hangman/model/GameScore.java
+++ src/main/java/hangman/model/GameScore.java
@@ -1,5 +1,9 @@
 package hangman.model;
 
+/**
+ * Interface about the diferente forms to calculate de game score
+ */
 public interface GameScore{
+    
     public int calculateScore(int correctCount , int incorrectCount );
 }
