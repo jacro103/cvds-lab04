2f879ecd997043e751323b81b75d9afd20c924f4
unknown <jose.correa-r@labinfo.is.escuelaing.edu.co>
2023-02-16 17:40:07 -0500

    implementacion pruebas

--- src/test/java/hangman/GameScoreTest.java
+++ src/test/java/hangman/GameScoreTest.java
@@ -1,5 +1,21 @@
 package hangman;
 
 public class GameScoreTest {
+    /**
+     * OriginalScore: 
+     * @pre 100 puntos
+     * @pos -10 letras incorrectas
+     * @pos 0 min puntaje
+     * PowerScore:
+     * @pre 0 puntos
+     * @pos -8 letras incorrectas
+     * @pos 500 puntaje maximo
+     * @pos 0 min puntaje 
+     * BonusScore:
+     * @pre 0 puntos
+     * @pos -5 letras incorrectas
+     * @pos +10 letras correctas
+     * @pos 0 min puntaje
+     */
     
 }
