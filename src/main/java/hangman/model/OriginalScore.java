c50b1ab1e6955b35c9f15681b0109c7fae00fea3
unknown <sebastian.blanco@labinfo.is.escuelaing.edu.co>
2023-02-16 17:28:20 -0500

    Se agregan los comentarios @pre @pos @param @throws

--- src/main/java/hangman/model/OriginalScore.java
+++ src/main/java/hangman/model/OriginalScore.java
@@ -2,6 +2,13 @@ package hangman.model;
 
 public class OriginalScore implements GameScore{
 
+    /**
+     * @pre
+     * @pos
+     * @param correctCount puntaje de letras acertadas
+     * @param incorrectCount puntaje de letras no acertadas
+     * @throws
+     */
     @Override
     public int calculateScore(int correctCount, int incorrectCount) {
         return 0;
