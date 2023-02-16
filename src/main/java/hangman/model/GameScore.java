edd762eee4cee68c06408b28d4729c57e57f5f0c
unknown <jose.correa-r@labinfo.is.escuelaing.edu.co>
2023-02-16 17:26:07 -0500

    2.0

--- src/main/java/hangman/model/GameScore.java
+++ src/main/java/hangman/model/GameScore.java
@@ -2,6 +2,8 @@ package hangman.model;
 
 /**
  * Interface about the diferente forms to calculate de game score
+ * @param correctCount puntaje de letras acertadas
+ * @param incorrectCount puntaje de letras no acertadas
  */
 public interface GameScore{
     
